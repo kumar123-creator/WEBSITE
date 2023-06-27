2463b564aa1b5dbb003d49c5266d1dc90df06c5e
kumar123-creator <132423913+kumar123-creator@users.noreply.github.com>
2023-06-27 15:03:28 +0530

    Update recruitingpricing.svelte

--- src/recruitingpricing.svelte
+++ src/recruitingpricing.svelte
@@ -4,48 +4,44 @@
   import { Link } from "svelte-routing";
   import Popup from './Popup.svelte';
 
-let isPopupOpen = false;
+  let isPopupOpen = false;
 
-const dispatch = createEventDispatcher();
+  const dispatch = createEventDispatcher();
 
-function openWhyRecruitlyPopup() {
-isPopupOpen = true;
-}
+  function openWhyRecruitlyPopup() {
+    isPopupOpen = true;
+  }
 
-function closePopup() {
-isPopupOpen = false;
-}
-dispatch('openWhyRecruitlyPopup', { closePopup });
-  
-  
-  </script>
-   {#if isPopupOpen}
-   <Popup {closePopup} />
- {/if}
-  
-  <section class="Recruitly Pricing">
-    <div class="links">
-        <nav>
-            <ul>
-               <a on:click={openWhyRecruitlyPopup} class="why-recruitly-anchor" style="color:blue; font-weight: 700;">Why Recruitly</a>
-              <span class="spacer"></span>
-              <li><Link to="/automation" style="color:blue; font-weight: 700;">Automation</Link></li>
-              <span class="spacer"></span>
-              <li><Link to="/pricing" style="color: blue; font-weight: 700;">Pricing</Link></li>
-             
-              
-         </ul>
-          </nav>
-          </div>
+  function closePopup() {
+    isPopupOpen = false;
+  }
+  dispatch('openWhyRecruitlyPopup', { closePopup });
+</script>
 
+{#if isPopupOpen}
+  <Popup {closePopup} />
+{/if}
 
-    <div class="pricing-component">
-      <div class="hero-section">
+<section class="RecruitlyPricing">
+  <div class="links">
+    <nav>
+      <ul>
+        <a on:click={openWhyRecruitlyPopup} class="why-recruitly-anchor" style="color: blue; font-weight: 700;">Why Recruitly</a>
+        <span class="spacer"></span>
+        <li><Link to="/automation" style="color: blue; font-weight: 700;">Automation</Link></li>
+        <span class="spacer"></span>
+        <li><Link to="/pricing" style="color: blue; font-weight: 700;">Pricing</Link></li>
+      </ul>
+    </nav>
+  </div>
+
+  <div class="pricing-component">
+    <div class="hero-section">
       <h1 style="color: blue;">Recruitly Pricing</h1>
       <p>Recruitly is a pay-as-you-go service, excluding 1-Week FREE trial. All payments should be made upfront before using the service.</p>
     </div>
 
- <section class="compare-section">
+    <section class="compare-section">
       <div class="container-fluid">
         <h1 style="color: red;">Compare Plans</h1>
         <table class="table table-bordered">
@@ -92,111 +88,52 @@ dispatch('openWhyRecruitlyPopup', { closePopup });
       </div>
     </section>
 
-      <section class="features-section">
-        <div class="container-fluid mt-4">
-          <h1 style="color:orange;">Affordable plans</h1>
+    <section class="features-section">
+      <div class="container-fluid mt-4">
+        <h1 style="color: orange;">Affordable plans</h1>
          
-          <div class="row">
-            <div class="col p-3 bg-info text-white" style="color: blue; font-weight: 700;">
-              <h1>Solo</h1>
-              <p>For independent recruiters</p>
-              <p>$35</p>
-              <p>/user/month</p>
-            </div>
+        <div class="row">
+          <div class="col p-3 bg-info text-white" style="color: blue; font-weight: 700;">
+            <h1>Solo</h1>
+            <p>For independent recruiters</p>
+            <p>$35</p>
+            <p>/user/month</p>
+          </div>
               
-            <div class="col p-3 bg-success  text-white" style=" font-weight: 700;">
-              <h1>Startup</h1>
-              <p>For startup agencies</p>
-              <p>$50</p>
-              <p>/user/month</p>
-            </div>
-            <div class="col p-3  bg-info text-white" style=" font-weight: 700;">
-              <h1>Professional
-              </h1>
-              <p>For established teams</p>
-              <p>$75</p>
-              <p>/user/month</p>
-            </div>
-            <div class="col p-3 bg-success  text-white" style="font-weight: 700;">
-              <h1>Enterprise
-              </h1>
-              <p>For large organisations</p>
-              <p>$100</p>
-              <p>/user/month</p>
-            </div>
+          <div class="col p-3 bg-success text-white" style="font-weight: 700;">
+            <h1>Startup</h1>
+            <p>For startup agencies</p>
+            <p>$50</p>
+            <p>/user/month</p>
+          </div>
+          <div class="col p-3 bg-info text-white" style="font-weight: 700;">
+            <h1>Professional</h1>
+            <p>For established teams</p>
+            <p>$75</p>
+            <p>/user/month</p>
+          </div>
+          <div class="col p-3 bg-success text-white" style="font-weight: 700;">
+            <h1>Enterprise</h1>
+            <p>For large organizations</p>
+            <p>$100</p>
+            <p>/user/month</p>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
   </div>
 </section>
-  
-  <style>
-   
-  .container{
-    width: 160vh;
+
+<style>
+  .RecruitlyPricing {
+    padding-top: 50px;
   }
-   
-  
-    .features-section h1 {
-      color: blue;
-      font-weight: bold;
-      font-size: 2rem;
-      margin-top: 50px;
-    }
-  
-   
-    .close-button {
-      margin-top: 20px;
-      padding: 10px 20px;
-      background-color: purple;
-      color: white;
-      border: none;
-      border-radius: 5px;
-      font-size: 16px;
-      cursor: pointer;
-    }
-    body {
-      margin: 0;
-      padding: 0;
-      font-family: Arial, sans-serif;
-    }
 
-    .pricing-component {
-        margin-top:5vh;
-      width: 220vh;
-      height:250vh;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      background-color:white;
-      padding: 20px;
-      margin-left: 140vh;
-      margin-right: -50vh;
-    }
-
-    
-
-    .pricing-component p {
-      font-size: 18px;
-      margin-bottom: 10px;
-    }
-
-    .pricing-component ul {
-      list-style-type: disc;
-      margin-left: 20px;
-    }
-
-    .pricing-component li {
-      font-size: 16px;
-      margin-bottom: 5px;
-    }
-
-    .links {
+  .links {
     display: flex;
     justify-content: flex-end;
     position: absolute;
-    top:8px;
+    top: 8px;
     right: 10px;
   }
   
@@ -209,24 +146,45 @@ dispatch('openWhyRecruitlyPopup', { closePopup });
   .links li {
     margin-right: 5px;
   }
+  
   .spacer {
-	margin: 0 10px;
+    margin: 0 10px;
   }
-  .close-button {
-	  position: absolute;
-	  top: 0px;
-	  right: 1px;
-	  font-size: 24px;
-	  color: #f00f22;
-	  background: transparent;
-	  border: none;
-	  cursor: pointer;
-	}
-    .why-recruitly-anchor {
-	  cursor: pointer;
-	}
+  
+  .why-recruitly-anchor {
+    cursor: pointer;
+  }
 
- .hero-section {
+  .pricing-component {
+    margin-top: 5vh;
+    width: 220vh;
+    height: 250vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: white;
+    padding: 20px;
+    margin-left: 140vh;
+    margin-right: -50vh;
+  }
+
+  .pricing-component p {
+    font-size: 18px;
+    margin-bottom: 10px;
+  }
+
+  .pricing-component ul {
+    list-style-type: disc;
+    margin-left: 20px;
+  }
+
+  .pricing-component li {
+    font-size: 16px;
+    margin-bottom: 5px;
+  }
+
+  .hero-section {
     text-align: center;
     margin-bottom: 10px;
   }
@@ -240,7 +198,7 @@ dispatch('openWhyRecruitlyPopup', { closePopup });
     text-align: center;
     font-weight: bold;
     font-size: 3rem;
-    margin-top: 50px;
+    margin-top: 10px;
     margin-bottom: 10px;
   }
 
@@ -257,5 +215,15 @@ dispatch('openWhyRecruitlyPopup', { closePopup });
     text-align: center;
   }
 
-  </style>
-  
+  .features-section {
+    margin-top: 50px;
+  }
+
+  .features-section h1 {
+    color: blue;
+    font-weight: bold;
+    font-size: 2rem;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+</style>
