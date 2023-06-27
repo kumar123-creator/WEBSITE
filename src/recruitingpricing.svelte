e4ed6b10739041a58845932ed6632550152f19e0
kumar123-creator <132423913+kumar123-creator@users.noreply.github.com>
2023-06-27 13:16:58 +0530

    Update recruitingpricing.svelte

--- src/recruitingpricing.svelte
+++ src/recruitingpricing.svelte
@@ -47,27 +47,27 @@ dispatch('openWhyRecruitlyPopup', { closePopup });
           <h1 style="color:orange;">Affordable plans</h1>
          
           <div class="row">
-            <div class="col p-3 bg-success text-white" style="color: blue; font-weight: 700;">
+            <div class="col p-3 bg-info text-white" style="color: blue; font-weight: 700;">
               <h1>Solo</h1>
               <p>For independent recruiters</p>
               <p>$35</p>
               <p>/user/month</p>
             </div>
               
-            <div class="col p-3 bg-warning  text-white" style=" font-weight: 700;">
+            <div class="col p-3 bg-success  text-white" style=" font-weight: 700;">
               <h1>Startup</h1>
               <p>For startup agencies</p>
               <p>$50</p>
               <p>/user/month</p>
             </div>
-            <div class="col p-3  bg-success text-white" style=" font-weight: 700;">
+            <div class="col p-3  bg-info text-white" style=" font-weight: 700;">
               <h1>Professional
               </h1>
               <p>For established teams</p>
               <p>$75</p>
               <p>/user/month</p>
             </div>
-            <div class="col p-3 bg-warning  text-white" style="font-weight: 700;">
+            <div class="col p-3 bg-success  text-white" style="font-weight: 700;">
               <h1>Enterprise
               </h1>
               <p>For large organisations</p>
@@ -77,9 +77,56 @@ dispatch('openWhyRecruitlyPopup', { closePopup });
           </div>
         </div>
       </section>
-        
+        <section class="compare-section">
+      <div class="container-fluid">
+        <h1 style="color: blue;">Compare Plans</h1>
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Solo</th>
+              <th>Startup</th>
+              <th>Professional</th>
+              <th>Enterprise</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Recruitment CRM</td>
+              <td>✔</td>
+              <td>✔</td>
+              <td>✔</td>
+              <td>✔</td>
+            </tr>
+            <tr>
+              <td>Applicant Tracking System</td>
+              <td>✔</td>
+              <td>✔</td>
+              <td>✔</td>
+              <td>✔</td>
+            </tr>
+            <tr>
+              <td>Job Ad Distribution</td>
+              <td>✔</td>
+              <td>✔</td>
+              <td>✔</td>
+              <td>✔</td>
+            </tr>
+            <tr>
+              <td>Email Marketing</td>
+              <td>✔</td>
+              <td>✔</td>
+              <td>✔</td>
+              <td>✔</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-      
+    </section>
+  </div>
+</section>
+
+      </div>
   </section>
   
   <style>
@@ -183,7 +230,30 @@ dispatch('openWhyRecruitlyPopup', { closePopup });
     .why-recruitly-anchor {
 	  cursor: pointer;
 	}
-   
+
+ .compare-section {
+    margin-top: 50px;
+  }
+
+  .compare-section h1 {
+    color: blue;
+    font-weight: bold;
+    font-size: 2rem;
+    margin-top: 50px;
+  }
+
+  .compare-section table {
+    margin-top: 30px;
+  }
+
+  .compare-section th {
+    text-align: center;
+    font-weight: bold;
+  }
+
+  .compare-section td {
+    text-align: center;
+  }
   
   </style>
   
