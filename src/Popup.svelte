4152cb0bdf9f8401a2649113129c9704e4145579
kumar123-creator <132423913+kumar123-creator@users.noreply.github.com>
2023-06-27 18:24:32 +0530

    Update Popup.svelte

--- src/Popup.svelte
+++ src/Popup.svelte
@@ -1,46 +1,63 @@
-<!-- Popup.svelte -->
 <script>
-    export let closePopup;
-  </script>
-  
-  <div class="popup">
-    <h3>Why Recruitly?</h3>
-    
-	 <div class="popup" on:click={closePopup}>
-	  
-        <div class="popup-content">
-          <button class="close-button" on:click={closePopup}>&times;</button>
-          <div class="popup-section">
-            <h3>Email Marketing</h3>
-            <p class="custom-paragraph">Engage talent at the right time, promote your business to the right audience.</p>
-          </div>
-          <div class="popup-section">
-            <h3>Applicant Tracking System</h3>
-            <p class="custom-paragraph">Candidate sharing, boolean searches, interview scheduling, automation and more.</p>
-          </div>
-          <div class="popup-section">
-            <h3>Website Integration</h3>
-            <p class="custom-paragraph">Turn your WordPress website into a fully featured job-board using our FREE plugins.</p>
-          </div>
-          <div class="popup-section">
-            <h3>Job Distribution</h3>
-            <p class="custom-paragraph">Get your job in front of millions of candidates, manage applicants in one place.</p>
-          </div>
-          <div class="popup-section">
-            <h3>Search & Import</h3>
-            <p class="custom-paragraph">Search and import candidates from CV-Library and TotalJobs with just a few clicks.</p>
-          </div>
-          <div class="popup-section">
-            <h3>Recruiting Automation</h3>
-            <p class="custom-paragraph">Boost your team's productivity by creating workflows that automate repetitive and common tasks.</p>
-          </div>
-        </div>
+  import { Link } from 'svelte-routing';
+
+  export let closePopup;
+</script>
+
+<div class="popup">
+  <h3>Why Recruitly?</h3>
+  <div class="popup" on:click={closePopup}>
+    <div class="popup-content">
+      <button class="close-button" on:click={closePopup}>&times;</button>
+      <div class="popup-section">
+        <Link to="/email-marketing"><h3>Email Marketing</h3></Link>
+        <p class="custom-paragraph">Engage talent at the right time, promote your business to the right audience.</p>
       </div>
-    <button on:click={closePopup}>Close</button>
+      <div class="popup-section">
+        <Link to="/applicant-tracking-system"><h3>Applicant Tracking System</h3></Link>
+        <p class="custom-paragraph">Candidate sharing, boolean searches, interview scheduling, automation and more.</p>
+      </div>
+      <div class="popup-section">
+        <Link to="/website-integration"><h3>Website Integration</h3></Link>
+        <p class="custom-paragraph">Turn your WordPress website into a fully featured job-board using our FREE plugins.</p>
+      </div>
+      <div class="popup-section">
+        <Link to="/job-distribution"><h3>Job Distribution</h3></Link>
+        <p class="custom-paragraph">Get your job in front of millions of candidates, manage applicants in one place.</p>
+      </div>
+      <div class="popup-section">
+        <Link to="/search-and-import"><h3>Search & Import</h3></Link>
+        <p class="custom-paragraph">Search and import candidates from CV-Library and TotalJobs with just a few clicks.</p>
+      </div>
+      <div class="popup-section">
+        <Link to="/recruiting-automation"><h3>Recruiting Automation</h3></Link>
+        <p class="custom-paragraph">Boost your team's productivity by creating workflows that automate repetitive and common tasks.</p>
+      </div>
+    </div>
   </div>
-  
-  <style>
-   
+  <button on:click={closePopup}>Close</button>
+</div>
+
+<style>
+  /* Popup styles */
+  /* ... */
+
+  .popup-section {
+    width: calc(50% - 20px);
+    height: 25%;
+    padding: 10px;
+  }
+
+  .popup-content h3 {
+    text-align: center;
+    color: blue;
+  }
+
+  .popup-content p.custom-paragraph {
+    width: 100%;
+    text-align: center;
+  }
+
     .popup {
 	  position: fixed;
 	  top: 0;
@@ -98,5 +115,4 @@
 	  border: none;
 	  cursor: pointer;
 	}
-  </style>
-  
+</style>
