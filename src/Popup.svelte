e83e94aa5063537d0541f4811b479c38863486f1
kumar123-creator <132423913+kumar123-creator@users.noreply.github.com>
2023-06-28 10:54:55 +0530

    Update Popup.svelte

--- src/Popup.svelte
+++ src/Popup.svelte
@@ -1,12 +1,12 @@
 <script>
-  export let closePopup;
+  import { Link, Route } from "svelte-routing";
 </script>
 
 <div class="popup" on:click={closePopup}>
   <div class="popup-content">
     <button class="close-button" on:click={closePopup}>&times;</button>
     <div class="popup-section">
-      <Route path="/email-marketing">
+      <Route path="/">
         <Link to="/email-marketing">
           <h3>Email Marketing</h3>
         </Link>
@@ -14,7 +14,7 @@
       <p class="custom-paragraph">Engage talent at the right time, promote your business to the right audience.</p>
     </div>
     <div class="popup-section">
-      <Route path="/applicant-tracking-system">
+      <Route path="/">
         <Link to="/applicant-tracking-system">
           <h3>Applicant Tracking System</h3>
         </Link>
@@ -22,7 +22,7 @@
       <p class="custom-paragraph">Candidate sharing, boolean searches, interview scheduling, automation and more.</p>
     </div>
     <div class="popup-section">
-      <Route path="/website-integration">
+      <Route path="/">
         <Link to="/website-integration">
           <h3>Website Integration</h3>
         </Link>
@@ -30,7 +30,7 @@
       <p class="custom-paragraph">Turn your WordPress website into a fully featured job-board using our FREE plugins.</p>
     </div>
     <div class="popup-section">
-      <Route path="/job-distribution">
+      <Route path="/">
         <Link to="/job-distribution">
           <h3>Job Distribution</h3>
         </Link>
@@ -38,7 +38,7 @@
       <p class="custom-paragraph">Get your job in front of millions of candidates, manage applicants in one place.</p>
     </div>
     <div class="popup-section">
-      <Route path="/search-import">
+      <Route path="/">
         <Link to="/search-import">
           <h3>Search & Import</h3>
         </Link>
@@ -46,7 +46,7 @@
       <p class="custom-paragraph">Search and import candidates from CV-Library and TotalJobs with just a few clicks.</p>
     </div>
     <div class="popup-section">
-      <Route path="/recruiting-automation">
+      <Route path="/">
         <Link to="/recruiting-automation">
           <h3>Recruiting Automation</h3>
         </Link>
@@ -55,7 +55,8 @@
     </div>
   </div>
 </div>
-  
+
+
   <style>
    
     .popup {
