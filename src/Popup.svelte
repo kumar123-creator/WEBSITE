1bc0ba5b1b4b7df5b68c0a101daab8c58a11d103
kumar123-creator <132423913+kumar123-creator@users.noreply.github.com>
2023-06-28 12:27:09 +0530

    Update Popup.svelte

--- src/Popup.svelte
+++ src/Popup.svelte
@@ -1,62 +1,43 @@
 <script>
-  import { Link, Route } from "svelte-routing";
-</script>
-
-<div class="popup" on:click={closePopup}>
-  <div class="popup-content">
-    <button class="close-button" on:click={closePopup}>&times;</button>
-    <div class="popup-section">
-      <Route path="/">
-        <Link to="/email-marketing">
-          <h3>Email Marketing</h3>
-        </Link>
-      </Route>
-      <p class="custom-paragraph">Engage talent at the right time, promote your business to the right audience.</p>
-    </div>
-    <div class="popup-section">
-      <Route path="/">
-        <Link to="/applicant-tracking-system">
-          <h3>Applicant Tracking System</h3>
-        </Link>
-      </Route>
-      <p class="custom-paragraph">Candidate sharing, boolean searches, interview scheduling, automation and more.</p>
-    </div>
-    <div class="popup-section">
-      <Route path="/">
-        <Link to="/website-integration">
-          <h3>Website Integration</h3>
-        </Link>
-      </Route>
-      <p class="custom-paragraph">Turn your WordPress website into a fully featured job-board using our FREE plugins.</p>
-    </div>
-    <div class="popup-section">
-      <Route path="/">
-        <Link to="/job-distribution">
-          <h3>Job Distribution</h3>
-        </Link>
-      </Route>
-      <p class="custom-paragraph">Get your job in front of millions of candidates, manage applicants in one place.</p>
-    </div>
-    <div class="popup-section">
-      <Route path="/">
-        <Link to="/search-import">
-          <h3>Search & Import</h3>
-        </Link>
-      </Route>
-      <p class="custom-paragraph">Search and import candidates from CV-Library and TotalJobs with just a few clicks.</p>
-    </div>
-    <div class="popup-section">
-      <Route path="/">
-        <Link to="/recruiting-automation">
-          <h3>Recruiting Automation</h3>
-        </Link>
-      </Route>
-      <p class="custom-paragraph">Boost your team's productivity by creating workflows that automate repetitive and common tasks.</p>
-    </div>
+    export let closePopup;
+  </script>
+  
+  <div class="popup">
+    <h3>Why Recruitly?</h3>
+    
+	 <div class="popup" on:click={closePopup}>
+	  
+        <div class="popup-content">
+          <button class="close-button" on:click={closePopup}>&times;</button>
+          <div class="popup-section">
+            <h3>Email Marketing</h3>
+            <p class="custom-paragraph">Engage talent at the right time, promote your business to the right audience.</p>
+          </div>
+          <div class="popup-section">
+            <h3>Applicant Tracking System</h3>
+            <p class="custom-paragraph">Candidate sharing, boolean searches, interview scheduling, automation and more.</p>
+          </div>
+          <div class="popup-section">
+            <h3>Website Integration</h3>
+            <p class="custom-paragraph">Turn your WordPress website into a fully featured job-board using our FREE plugins.</p>
+          </div>
+          <div class="popup-section">
+            <h3>Job Distribution</h3>
+            <p class="custom-paragraph">Get your job in front of millions of candidates, manage applicants in one place.</p>
+          </div>
+          <div class="popup-section">
+            <h3>Search & Import</h3>
+            <p class="custom-paragraph">Search and import candidates from CV-Library and TotalJobs with just a few clicks.</p>
+          </div>
+          <div class="popup-section">
+            <h3>Recruiting Automation</h3>
+            <p class="custom-paragraph">Boost your team's productivity by creating workflows that automate repetitive and common tasks.</p>
+          </div>
+        </div>
+      </div>
+    <button on:click={closePopup}>Close</button>
   </div>
-</div>
-
-
+  
   <style>
    
     .popup {
@@ -117,4 +98,3 @@
 	  cursor: pointer;
 	}
   </style>
-  
