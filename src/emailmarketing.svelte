194905eea3bfbdc810cb45db5ac1ccda9bbd9ad1
kumar123-creator <132423913+kumar123-creator@users.noreply.github.com>
2023-06-28 15:25:07 +0530

    Update emailmarketing.svelte

--- src/emailmarketing.svelte
+++ src/emailmarketing.svelte
@@ -48,7 +48,7 @@
     <img src = "https://recruitly.io/wp-content/uploads/2020/11/email-templates@2x-1024x800.png" alt="Recruitly Email Templates" style="width: 100%; max-width: 500px; margin-bottom: 50px />
 
 
-<h2 style="color: darkmagenta;">Measure Performance</h2>
+<p style="color:green; font-weight: 900;">Measure Performance</p>
 <p style="color:black; font-weight: 700;">You can track everything from who opened and clicked your campaigns, health of your email lists and performance of your campaigns and segments.</p>
 <p style="color:black; font-weight: 600;">Gain valuable insights to shape your email marketing strategies.</p>
   
